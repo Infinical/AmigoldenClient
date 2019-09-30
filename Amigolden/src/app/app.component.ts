e670bcd7aa0245fd6b996a5a89be50641d251861
Granthoff1107 <Granthoff1107@gmail.com>
2019-09-30 19:56:57 -0400

    adding support for back and forward navigation during routing

--- Amigolden/src/app/app.component.ts
+++ Amigolden/src/app/app.component.ts
@@ -1,8 +1,10 @@
 import { Component } from '@angular/core';
-
+import { Router, NavigationStart } from '@angular/router';
+import { Event as NavigationEvent } from '@angular/router';
 import { Platform } from '@ionic/angular';
 import { SplashScreen } from '@ionic-native/splash-screen/ngx';
 import { StatusBar } from '@ionic-native/status-bar/ngx';
+import { filter } from 'rxjs/operators';
 
 @Component({
   selector: 'app-root',
@@ -51,9 +53,47 @@ export class AppComponent {
   constructor(
     private platform: Platform,
     private splashScreen: SplashScreen,
-    private statusBar: StatusBar
+    private statusBar: StatusBar,
+    private router: Router
   ) {
     this.initializeApp();
+    this.handleNavigation();
+  }
+
+  handleNavigation() {
+    this.router.events
+            .pipe(filter((event: NavigationEvent) => (event instanceof NavigationStart)))
+            .subscribe(( event: NavigationStart ) => {
+
+              console.group( 'NavigationStart Event' );
+              // Every navigation sequence is given a unique ID. Even "popstate"
+              // navigations are really just "roll forward" navigations that get
+              // a new, unique ID.
+              console.log('navigation id:', event.id);
+              console.log('route:', event.url);
+              // The "navigationTrigger" will be one of:
+              // --
+              // - imperative (ie, user clicked a link).
+              // - popstate (ie, browser controlled change such as Back button).
+              // - hashchange
+              // --
+              // NOTE: I am not sure what triggers the "hashchange" type.
+              console.log('trigger:', event.navigationTrigger);
+
+              // This "restoredState" property is defined when the navigation
+              // event is triggered by a "popstate" event (ex, back / forward
+              // buttons). It will contain the ID of the earlier navigation event
+              // to which the browser is returning.
+              // --
+              // CAUTION: This ID may not be part of the current page rendering.
+              // This value is pulled out of the browser; and, may exist across
+              // page refreshes.
+              if ( event.restoredState ) {
+                this.router.getCurrentNavigation().extras.state = event.restoredState;
+                console.warn('restoring navigation id:', event.restoredState.navigationId);
+              }
+              console.groupEnd();
+            });
   }
 
   initializeApp() {
