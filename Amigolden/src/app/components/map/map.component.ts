11ffff9c0121bc3552cd8a04d70a585b5219c851
Infinical <inficalvin@gmail.com>
2020-06-29 13:01:24 +0300

    Configure slider

--- Amigolden/src/app/components/map/map.component.ts
+++ Amigolden/src/app/components/map/map.component.ts
@@ -15,6 +15,8 @@ import { Location } from 'src/app/models/location';
 import { MapOptions } from 'src/app/models/map/map-options';
 import * as _ from 'underscore';
 import { FormGroup, FormBuilder } from '@angular/forms';
+import { IonSlides } from '@ionic/angular';
+
 declare var google: any;
 
 @Component({
@@ -52,7 +54,69 @@ export class MapComponent implements OnInit {
   private geoCoder;
 
   @ViewChild('search', { static: false })
+  @ViewChild(IonSlides, { static: false })
+  slides: IonSlides;
   public searchElementRef: ElementRef;
+
+  slideOpts = {
+    on: {
+      beforeInit() {
+        const swiper = this;
+        swiper.classNames.push(`${swiper.params.containerModifierClass}fade`);
+        const overwriteParams = {
+          slidesPerView: 1,
+          slidesPerColumn: 1,
+          slidesPerGroup: 1,
+          watchSlidesProgress: true,
+          spaceBetween: 0,
+          virtualTranslate: true,
+        };
+        swiper.params = Object.assign(swiper.params, overwriteParams);
+        swiper.params = Object.assign(swiper.originalParams, overwriteParams);
+      },
+      setTranslate() {
+        const swiper = this;
+        const { slides } = swiper;
+        for (let i = 0; i < slides.length; i += 1) {
+          const $slideEl = swiper.slides.eq(i);
+          const offset$$1 = $slideEl[0].swiperSlideOffset;
+          let tx = -offset$$1;
+          if (!swiper.params.virtualTranslate) tx -= swiper.translate;
+          let ty = 0;
+          if (!swiper.isHorizontal()) {
+            ty = tx;
+            tx = 0;
+          }
+          const slideOpacity = swiper.params.fadeEffect.crossFade
+            ? Math.max(1 - Math.abs($slideEl[0].progress), 0)
+            : 1 + Math.min(Math.max($slideEl[0].progress, -1), 0);
+          $slideEl
+            .css({
+              opacity: slideOpacity,
+            })
+            .transform(`translate3d(${tx}px, ${ty}px, 0px)`);
+        }
+      },
+      setTransition(duration) {
+        const swiper = this;
+        const { slides, $wrapperEl } = swiper;
+        slides.transition(duration);
+        if (swiper.params.virtualTranslate && duration !== 0) {
+          let eventTriggered = false;
+          slides.transitionEnd(() => {
+            if (eventTriggered) return;
+            if (!swiper || swiper.destroyed) return;
+            eventTriggered = true;
+            swiper.animating = false;
+            const triggerEvents = ['webkitTransitionEnd', 'transitionend'];
+            for (let i = 0; i < triggerEvents.length; i += 1) {
+              $wrapperEl.trigger(triggerEvents[i]);
+            }
+          });
+        }
+      },
+    },
+  };
 
   constructor(
     private mapsAPILoader: MapsAPILoader,
@@ -111,6 +175,15 @@ export class MapComponent implements OnInit {
   }
 
   cancel() {}
+
+  // slider functions
+
+  slidePrev() {
+    this.slides.slidePrev();
+  }
+  slideNext() {
+    this.slides.slideNext();
+  }
 
   ngOnInit() {
     // load Places Autocomplete
@@ -236,7 +309,7 @@ export class MapComponent implements OnInit {
         if (status === 'OK') {
           if (results[0]) {
             this.zoom = 12;
-            console.log(results[0].formatted_address[0]);
+
             // this.address = results[0].formatted_address;
             this.address = results[0].address_components[0].short_name;
             this.town = results[0].address_components[1].short_name;
